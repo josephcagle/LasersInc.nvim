f22cff485bc1c2f78dbeb1ff1b0f36ff65dca21a
Joseph Cagle <joseph@josephcagle.com>
2022-06-13 19:44:46 -0400

    Add some music controls

--- rplugin/python3/sound/sound.py
+++ rplugin/python3/sound/sound.py
@@ -13,9 +13,26 @@ class SoundManager:
             "shoot_bullet": mixer.Sound("data/sound/shoot_bullet.wav"),
             "explosion1": mixer.Sound("data/sound/explosion1.wav")
         }
+        self.music = {
+            "main_menu": "data/sound/LasersIncMenu.mp3"
+        }
 
     def play_sound(self, sound_name):
         if sound_name not in self.sounds:
             raise RuntimeError(f"sound not found: {sound_name}")
         mixer.Sound.play(self.sounds[sound_name])
+
+    def load_music(self, music_name):
+        mixer.music.load(self.music[music_name])
+    def play_music(self, loop=False):
+        mixer.music.play(-1 if loop else 0)
+    def pause_music(self):
+        mixer.music.pause()
+    def unpause_music(self):
+        mixer.music.unpause()
+    def is_music_playing(self):
+        return mixer.music.get_busy()
+    def set_music_volume(self, volume_fraction):
+        mixer.music.set_volume(volume_fraction)
+
 
